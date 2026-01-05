52fd5e722c6ddb85ad2913409b816b4e19de77c4
tinnerhrhe <hhebb@connect.ust.hk>
2026-01-05 18:01:02 +0800

    update

--- flow_grpo/stat_tracking.py
+++ flow_grpo/stat_tracking.py
@@ -1,7 +1,92 @@
 import numpy as np
 from collections import deque
 import torch
+class PerPromptStatTrackerNFT:
+    def __init__(self, global_std=False):
+        self.global_std = global_std
+        self.stats = {}
+        self.history_prompts = set()
 
+    # exp reward is for rwr
+    def update(self, prompts, rewards, dists=None, exp=False):
+        prompts = np.array(prompts)
+        rewards = np.array(rewards, dtype=np.float64)
+        dists = np.array(dists)
+        dists = dists / np.linalg.norm(dists, ord=2, axis=1, keepdims=True)
+        unique = np.unique(prompts)
+        advantages = np.empty_like(rewards) * 0.0
+        for prompt in unique:
+            prompt_rewards = rewards[prompts == prompt]
+            # prompt_dists = dists[prompts == prompt]
+            if prompt not in self.stats:
+                self.stats[prompt] = []
+            self.stats[prompt].extend(prompt_rewards)
+            self.history_prompts.add(hash(prompt))  # Add hash of prompt to history_prompts
+        for prompt in unique:
+            self.stats[prompt] = np.stack(self.stats[prompt])
+            prompt_rewards = rewards[prompts == prompt]  # Fix: Recalculate prompt_rewards for each prompt
+            prompt_dists = dists[prompts == prompt]
+            similarity_matrix = np.dot(prompt_dists, prompt_dists.T).clip(-1,1)
+            distance_matrix = 1 - similarity_matrix
+            avg_distances = np.zeros(len(distance_matrix))
+            for i in range(len(distance_matrix)):
+                mask = np.ones(len(distance_matrix), dtype=bool)
+                mask[i] = False
+                other_distances = distance_matrix[i, mask]
+                avg_distances[i] = np.min(other_distances)
+            # min_val = np.min(avg_distances)
+            # max_val = np.max(avg_distances)
+            prompt_dists = avg_distances#/np.mean(avg_distances)#(avg_distances - min_val) / (max_val - min_val)
+            prompt_dists = np.repeat(prompt_dists[:, np.newaxis],prompt_rewards.shape[-1], axis=1)
+            prompt_rewards = prompt_rewards
+            mean = np.mean(self.stats[prompt], axis=0, keepdims=True)
+            prompt_dists = np.where((prompt_rewards - mean)<0,np.ones_like(prompt_dists),prompt_dists/np.mean(prompt_dists))
+            if self.global_std:
+                std = np.std(rewards, axis=0, keepdims=True) + 1e-4  # Use global std of all rewards
+            else:
+                std = np.std(self.stats[prompt], axis=0, keepdims=True) + 1e-4
+            advantages[prompts == prompt] = (prompt_rewards - mean) * (prompt_dists)# / std
+        return advantages,prompt_dists
+    def get_stats(self):
+        avg_group_size = sum(len(v) for v in self.stats.values()) / len(self.stats) if self.stats else 0
+        history_prompts = len(self.history_prompts)
+        return avg_group_size, history_prompts
+
+    def clear(self):
+        self.stats = {}
+
+    def get_mean_of_top_rewards(self, top_percentage):
+        if not self.stats:
+            return 0.0
+
+        assert 0 <= top_percentage <= 100
+
+        per_prompt_top_means = []
+        for prompt_rewards in self.stats.values():
+            if isinstance(prompt_rewards, list):
+                rewards = np.array(prompt_rewards)
+            else:
+                rewards = prompt_rewards
+
+            if rewards.size == 0:
+                continue
+
+            if top_percentage == 100:
+                per_prompt_top_means.append(np.mean(rewards))
+                continue
+
+            lower_bound_percentile = 100 - top_percentage
+            threshold = np.percentile(rewards, lower_bound_percentile)
+
+            top_rewards = rewards[rewards >= threshold]
+
+            if top_rewards.size > 0:
+                per_prompt_top_means.append(np.mean(top_rewards))
+
+        if not per_prompt_top_means:
+            return 0.0
+
+        return np.mean(per_prompt_top_means)
 class PerPromptStatTracker:
     def __init__(self, global_std=False):
         self.global_std = global_std
